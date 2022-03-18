5d4d775267fa62ad7d253f370c0933a21851ca40
Hyerim <sinhyerim@gmail.com>
2022-03-17 18:15:31 -0700

    Changed design

--- app/src/main/java/com/example/room_wrangler/MainActivity.java
+++ app/src/main/java/com/example/room_wrangler/MainActivity.java
@@ -1,17 +1,42 @@
 package com.example.room_wrangler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        // Configure sign-in to request the user's ID, email address, and basic
+//        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//        // Build a GoogleSignInClient with the options specified by gso.
+//        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
         goToMainPage();//Test main_page!!!
+
     }
 
     //Test main_page!!!
@@ -19,4 +44,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainPageActivity.class);
         startActivity(intent);
     }
+
+
 }
