567a622dec1fb32f05f5a67b74449058d85baa30
michael-gagno <evilmike@gmail.com>
2022-03-22 11:19:42 -0700

    Remove some unneedes files

--- app/src/main/java/com/example/room_wrangler/RoomInfoActivity.java
+++ app/src/main/java/com/example/room_wrangler/RoomInfoActivity.java
@@ -9,6 +9,7 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +30,7 @@ public class RoomInfoActivity extends AppCompatActivity {
         setUpRoom(room);
         setUpDate();
         setUpSlidingTimeSlots();
+        setUpBookingButton();
     }
 
     //Display pic and description
@@ -105,5 +107,15 @@ public class RoomInfoActivity extends AppCompatActivity {
         LinearSnapHelper snapHelper = new LinearSnapHelper();
         snapHelper.attachToRecyclerView(recyclerView);
         recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private void setUpBookingButton() {
+        Button button = findViewById(R.id.button_book_room);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
