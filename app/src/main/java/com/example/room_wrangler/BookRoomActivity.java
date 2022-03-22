8a6b0bd91cc486e3050e21e0009ce262c4317917
michael-gagno <evilmike@gmail.com>
2022-03-22 11:05:37 -0700

    Add files for room bookimng

---
+++ app/src/main/java/com/example/room_wrangler/BookRoomActivity.java
@@ -0,0 +1,14 @@
+package com.example.room_wrangler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_room);
+    }
+}
